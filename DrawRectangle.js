384dc49617938462a546f4e4b918cd6e81bf2f91
Edward Gurley IV <edward.gurley@loop.colum.edu>
2018-09-16 21:48:09 -0500

    pre

---
+++ DrawRectangle.js
@@ -0,0 +1,15 @@
+//DrawRectangle.js
+function main() {
+    //Retrieve <canvas> element
+    var canvas = document.getElementById('example');
+    if (!canvas) {
+        console.log('Failed to retrieve the <canvas> element');
+        return;
+    }
+    //Get the rendering conntext for 2DCG
+    var ctx = canvas.getContext('2d');
+
+    //Draw a blue rectangle
+    ctx.fillStyle = 'rgba(0, 0, 255, 1.0)'; //Set blue color
+    ctx.fillRect(120, 10, 150, 150); //Fill rectangle with the color
+}
